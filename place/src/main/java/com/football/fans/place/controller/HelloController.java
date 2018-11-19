ea308f1e34697967baaaf41f40662b8ac7c53e04
zqyang0719 <zqyang0719@163.com>
2018-11-19 17:24:50 +0800

    提交

--- place/src/main/java/com/football/fans/place/controller/HelloController.java
+++ place/src/main/java/com/football/fans/place/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.football.fans.place.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${hello.word}")
     public String words;
